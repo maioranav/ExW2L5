ddcd3e47b977d679ec69dacdaa679219c8adc0ec
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-24 13:28:06 +0100

    save to file method ok

--- src/main/java/org/vm93/Main.java
+++ src/main/java/org/vm93/Main.java
@@ -1,14 +1,15 @@
 package org.vm93;
 
+import org.apache.commons.io.FileUtils;
 import org.vm93.biblioteca.Libro;
 import org.vm93.biblioteca.Periodic;
 import org.vm93.biblioteca.Pubblicazione;
 import org.vm93.biblioteca.Rivista;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
 
@@ -16,10 +17,10 @@ public class Main {
 
     private static List<Pubblicazione> libreria = new ArrayList<>();
 
-    public static void main(String[] args) {
-        libreria.add(new Libro((long) 125498515, "Il passante", 2020, (short) 12, "Vincenzo", "Horror"));
-        libreria.add(new Rivista((long) 1456196196, "Gesu è risorto", 2023, (short) 32, Periodic.SETTIMANALE));
-        System.out.println(libreria.toString());
+    public static void main(String[] args) throws FileNotFoundException {
+        //  libreria.add(new Libro((long) 125498515, "Il passante", 2020, (short) 12, "Vincenzo", "Horror"));
+        //  libreria.add(new Rivista((long) 1456196196, "Gesu è risorto", 2023, (short) 32, Periodic.SETTIMANALE));
+        //  System.out.println(libreria.toString());
 
         System.out.println("\n** BIBLIOTECA DI VINCENZO **");
 
@@ -30,6 +31,7 @@ public class Main {
             System.out.println("\t 3 - RICERCA PER ANNO DI PUBBLICAZIONE");
             System.out.println("\t 4 - RICERCA PER AUTORE");
             System.out.println("\t 5 - MOSTRA TUTTO");
+            System.out.println("\t 6 - BACKUP E RIPRISTINO");
             System.out.println("\t 0 - ESCI");
 
             switch (scan.nextInt()) {
@@ -82,6 +84,32 @@ public class Main {
                     System.out.println("\n\t >> MOSTRO TUTTI I LIBRI IN LIBRERIA");
                     System.out.println(libreria.toString());
                 }
+                case 6 -> {
+                    System.out.println("\n\tBACKUP E RIPRISTINO\n\t 1-SALVA\t2-RIPRISTINA");
+                    switch (scan.nextInt()) {
+                        case 1 -> {
+                            System.out.println("=> INSERIAMO NOME DEL FILE");
+                            scan.nextLine();
+                            String nomefile = scan.nextLine();
+                            File f = new File(nomefile);
+                            try {
+                                FileUtils.writeStringToFile(f, libreria.toString(), "UTF-8");
+                                System.out.println("\n-> Ho scritto i valori sul file" + f);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        case 2 -> {
+                            System.out.println("=> INSERIAMO NOME DEL FILE");
+                            scan.nextLine();
+                            String nomefile = scan.nextLine();
+                            readFromFile(nomefile);
+                        }
+                        default -> {
+                            System.out.println("Scelta non valida, riprova \n");
+                        }
+                    }
+                }
                 case 0 -> {
                     System.out.println("\nQuesto programma è stato sviluppato da Vincenzo Maiorana");
                     System.out.println("\nPer ulteriori informazioni visita https://www.vincenzomaiorana.it");
@@ -96,5 +124,48 @@ public class Main {
 
     }
 
+    public static void readFromFile(String filename) {
+
+        try {
+            // Crea un oggetto BufferedReader per leggere il file
+            FileInputStream fis = new FileInputStream("input.txt");
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+
+            // Legge la prima riga del file
+            String input = br.readLine().substring(1);// Legge la riga iniziale "["
+
+            // Legge gli oggetti dal file e li aggiunge all'ArrayList
+            String[] oggetti = input.split(", "); // Divide la stringa in un array di oggetti
+            for (String oggetto : oggetti) {
+                if (oggetto.startsWith("Libro")) {
+                    String[] attributi = oggetto.substring(7, oggetto.length() - 1).split(", "); // Divide la stringa in un array di attributi
+                    String author = attributi[0].substring(8);
+                    String title = attributi[1].substring(7);
+                    int year = Integer.parseInt(attributi[2].substring(5));
+                    String genre = attributi[3].substring(6);
+                    short pages = Short.parseShort(attributi[4].substring(6));
+                    long isbn = Long.parseLong(attributi[5].substring(6));
+                    libreria.add(new Libro(isbn, title, year, pages, author, genre));
+                } else if (oggetto.startsWith("Rivista")) {
+                    String[] attributi = oggetto.substring(9, oggetto.length() - 1).split(", "); // Divide la stringa in un array di attributi
+                    String title = attributi[0].substring(7);
+                    Periodic periods = Periodic.valueOf(attributi[1].substring(8));
+                    int year = Integer.parseInt(attributi[2].substring(5));
+                    short pages = Short.parseShort(attributi[3].substring(6));
+                    long isbn = Long.parseLong(attributi[4].substring(6));
+                    libreria.add(new Rivista(isbn, title, year, pages, periods));
+                }
+            }
+
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
